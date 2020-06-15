e17645f17f732c26fd65924cb6758f33708e14c8
wicker <bwicker@cedarville.edu>
2020-06-15 10:25:47 -0400

    build artifacts. Need to remove later

--- DesktopCrawler/Program.cs
+++ DesktopCrawler/Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Threading;
+﻿using System.Threading;
 using Common;
 
 namespace DesktopCrawler
@@ -11,12 +10,17 @@ namespace DesktopCrawler
             var username = "";
             var password = "";
 
+            if (args.Length > 0) {
+                username = args[0];
+                password = args[1];
+            }
+
             using(var driver = Driver.GetDesktopDriver()) {
                 var bingPage = new BingPage(driver);
                 var searches = Driver.GetSearches();
 
                 Thread.Sleep(2000);
-                bingPage.PerformSignIn();
+                bingPage.PerformSignIn(username, password);
                 driver.Navigate().GoToUrl("https://www.bing.com/search?q=hello&cvid=3087b4535abe40b2b41d13f4d462c7fd&FORM=ANSPA1&PC=U531");
 
                 foreach(var searchTerm in searches) {
